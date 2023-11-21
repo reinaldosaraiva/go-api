f598fad5b3dc1b91aac114e82326d69f2b0412d1
Reinaldo Saraiva do Carmo <reinaldo.saraiva@gmail.com>
2023-11-20 21:58:08 -0300

    Add: Documentation Swagger

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -9,13 +9,27 @@ import (
 	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/jwtauth"
 	"github.com/reinaldosaraiva/go-api/configs"
+	_ "github.com/reinaldosaraiva/go-api/docs"
 	"github.com/reinaldosaraiva/go-api/internal/entity"
 	"github.com/reinaldosaraiva/go-api/internal/infra/database"
 	"github.com/reinaldosaraiva/go-api/internal/infra/webserver/handlers"
+	httpSwagger "github.com/swaggo/http-swagger"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
+// @title	API Go
+// @version	1.0
+// @description	API Go with authentication JWT and CRUD
+// @termsOfService	http://swagger.io/terms
+// @contact.name	Reinaldo Saraiva
+// @contact.email	reinaldo.saraiva@gmail.com
+// license.name		Bearware
+// host				localhost:8000
+// @BasePath	/
+// @securityDefinitions.apikey	ApiKeyAuth
+// @in	header
+// @name	Authorization
 func main() {
 	config, err := configs.LoadConfig(".")
 	if err != nil {
@@ -47,6 +61,8 @@ func main() {
 	r.Post("/users", userHandler.CreateUser)
 
 	r.Post("/users/generate_token", userHandler.GetJWT)
+
+	r.Get("/docs/*", httpSwagger.Handler(httpSwagger.URL("http://localhost:8000/docs/swagger/doc.json")))
 
 	http.ListenAndServe(":"+config.WebServerPort, r)
 }
